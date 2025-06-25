d3994e74c7c170fe0a1bcec4cae79a877390077d
ArmandoSJ <just-jauregui@hotmail.com>
2025-06-25 11:25:01 -0600

    update initial structure

--- src/main/java/com/technologyos/solid/SolidApplication.java
+++ src/main/java/com/technologyos/solid/SolidApplication.java
@@ -1,7 +1,13 @@
 package com.technologyos.solid;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SolidApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidApplication.class, args);
+	}
+
 }
